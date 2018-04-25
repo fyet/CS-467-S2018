8b730614ed6a045541adf3ae20b0ea2570f9fd69
Matthew Morse <morsmatt@oregonstate.edu>
2018-04-25 13:38:51 -0800

    Added navigation to admin-side homepage.

--- public_html/components/navbar.php
+++ public_html/components/navbar.php
@@ -3,7 +3,7 @@
 ************************************************************************** -->
 <nav class="navbar navbar-dark bg-dark fixed-top">
   <!-- Start brand/logo element -->
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand" href="admin-home.php">
     <img src="images/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
     <!-- Start responsive heading -->
     <div class="d-sm-none d-inline">

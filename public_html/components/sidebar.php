a778c1bff69f34df8fb05b92310e4810494e41ed
Matthew Morse <morsmatt@oregonstate.edu>
2018-04-27 14:18:39 -0800

    Replaced HTML SVG elements with PHP include calls to the same SVG files.

--- public_html/components/sidebar.php
+++ public_html/components/sidebar.php
@@ -2,27 +2,27 @@
   <div class="btn-group-vertical">
     <!-- Home button -->
     <a class="btn btn-light" href="admin-home.php">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M16 9l-3-3V2h-2v2L8 1 0 9h2l1 5c0 .55.45 1 1 1h8c.55 0 1-.45 1-1l1-5h2zm-4 5H9v-4H7v4H4L2.81 7.69 8 2.5l5.19 5.19L12 14z"/></svg>
+      <?php include("images/Octicons/home.svg"); ?>
       <div class="sidebar-text">home</div>
     </a>
     <!-- Manage users button -->
     <a class="btn btn-light" href="manage-users.php">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M16 12.999c0 .439-.45 1-1 1H7.995c-.539 0-.994-.447-.995-.999H1c-.54 0-1-.561-1-1 0-2.634 3-4 3-4s.229-.409 0-1c-.841-.621-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.442.58 2.5 3c.058 2.41-.159 2.379-1 3-.229.59 0 1 0 1s1.549.711 2.42 2.088A6.78 6.78 0 0 1 10 8.999s.229-.409 0-1c-.841-.62-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.437.581 2.495 3c.059 2.41-.158 2.38-1 3-.229.59 0 1 0 1s3.005 1.366 3.005 4z"/></svg>
+      <?php include("images/Octicons/organization.svg"); ?>
       <div class="sidebar-text">manage users</div>
     </a>
     <!-- Manage admin button -->
     <a class="btn btn-light" href="manage-admin.php">
-      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M14 8.76v-1.6l-1.94-.64-.45-1.09.88-1.84-1.13-1.13-1.81.91-1.09-.45L7.77 1h-1.6l-.63 1.94-1.11.45-1.84-.88-1.13 1.13.91 1.81-.45 1.09L0 7.22v1.59l1.94.64.45 1.09-.88 1.84 1.13 1.13 1.81-.91 1.09.45.69 1.92h1.59l.63-1.94 1.11-.45 1.84.88 1.13-1.13-.92-1.81.47-1.09L14 8.74v.02zm-7 2.23c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"/></svg>
+      <?php include("images/Octicons/gear.svg"); ?>
       <div class="sidebar-text">manage admin</div>
     </a>
     <!-- Business insights button -->
-    <a class="btn btn-light" href="#">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M16 14v1H0V0h1v14h15zM5 13H3V8h2v5zm4 0H7V3h2v10zm4 0h-2V6h2v7z"/></svg>
+    <a class="btn btn-light" href="admin-biz.php">
+      <?php include("images/Octicons/graph.svg"); ?>
       <div class="sidebar-text">business insights</div>
     </a>
     <!-- Logout button -->
     <a class="btn btn-light" href="#">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M12 9V7H8V5h4V3l4 3-4 3zm-2 3H6V3L2 1h8v3h1V1c0-.55-.45-1-1-1H1C.45 0 0 .45 0 1v11.38c0 .39.22.73.55.91L6 16.01V13h4c.55 0 1-.45 1-1V8h-1v4z"/></svg>
+      <?php include("images/Octicons/sign-out.svg"); ?>
       <div class="sidebar-text">logout</div>
     </a>
   </div>

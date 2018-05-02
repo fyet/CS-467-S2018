568df204caecc53efb24f5e02e825bd608215780
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-02 15:44:44 -0800

    Add JS script to test Google Charts library with our own DB.

--- public_html/admin-biz.php
+++ public_html/admin-biz.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <title>Admin Home</title>
+  <title>Business Insights</title>
   <!-- Required meta tags for Bootstrap -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -28,16 +28,70 @@
                                     page content
       ********************************************************************** -->
       <div class="col-sm-12 page-content">
-        <!-- navigation cards -->
-
+        <!-- column chart -->
+        <div class="row" id="chart_div" style="width: 800px; height: 500px;"></div>
       </div>
     </div>
   </div> <!-- End sidebar and page content -->
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <!-- Google Charts library -->
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+  <script type="text/javascript">
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      // Create an empty data table
+      var data = new google.visualization.DataTable();
+
+      // Add column headings manually
+      data.addColumn('string', 'Last Name');
+      data.addColumn('number', 'Salary');
+
+
+      //Get data from BLS(TEST)
+      /*var blsData = $.ajax({
+        url: "BLSdata.php",
+        dataType: "json",
+        async: false
+      }).responseText;
+      console.log(blsData);*/
+
+      // Get row data from AwardHub DB
+      var jsonData = $.ajax({
+        url: "recipientData.php",
+        dataType: "json",
+        async: false
+      }).responseText;
+      //console.log(jsonData);
+      //Convert json string to array of objects
+      var obj = JSON.parse(jsonData);
+      var rowData = [];
+      for (var i = 0; i < obj.length; i++) {
+        var nextRow = [obj[i].l_name, Number(obj[i].salary)];
+        rowData.push(nextRow);
+      }
+      //Add db data to DataTable
+      data.addRows(rowData);
+      //Set chart options
+      var options = {
+              title: "Salaries of Award Recipients",
+              width: 800,
+              height: 500,
+              bar: {groupWidth: "95%"},
+              legend: { position: "none" },
+      };
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+      }
+
+  </script>
   <!-- Add custom scripts below-->
 </body>
 </html>

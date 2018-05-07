953d163b7c1b1116a069f2992577172dfb459d42
fyet <fyet@oregonstate.edu>
2018-05-06 23:00:29 -0700

    updated db config

--- public_html/config.php
+++ public_html/config.php
@@ -1,9 +1,9 @@
 <?php
 
-DEFINE('DB_USER','fyet-db');
-DEFINE('DB_PASSWORD','FZYLHbqfslqyA1WB');
-DEFINE('DB_HOST','oniddb.cws.oregonstate.edu');
-DEFINE('DB_NAME','fyet-db');
+DEFINE('DB_USER','root');
+DEFINE('DB_PASSWORD','camelopardalis');
+DEFINE('DB_HOST','http://18.188.194.159');
+DEFINE('DB_NAME','camelopardalis');
 
 $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
     or die("Could not connect to MySql Database: " . mysqli_connect_error());

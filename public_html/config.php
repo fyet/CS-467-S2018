b2e588c4ea8ec31f13da14cef08e60fc7e9d2536
fyet <fyet@oregonstate.edu>
2018-05-06 23:04:21 -0700

    updated db config file

--- public_html/config.php
+++ public_html/config.php
@@ -2,7 +2,7 @@
 
 DEFINE('DB_USER','root');
 DEFINE('DB_PASSWORD','camelopardalis');
-DEFINE('DB_HOST','http://18.188.194.159');
+DEFINE('DB_HOST','localhost');
 DEFINE('DB_NAME','camelopardalis');
 
 $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)

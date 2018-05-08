72c6ab4bb275dfeb8c5c3f251c145be507c8261e
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-07 17:20:32 -0800

    Local DB configuration

--- public_html/config.php
+++ public_html/config.php
@@ -1,14 +1,19 @@
 <?php
 
-DEFINE('DB_USER','fyet-db');
+DEFINE('DB_USER','root');
+DEFINE('DB_PASSWORD','root');
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_NAME','award_hub');
+
+/*DEFINE('DB_USER','fyet-db');
 DEFINE('DB_PASSWORD','FZYLHbqfslqyA1WB');
 DEFINE('DB_HOST','oniddb.cws.oregonstate.edu');
-DEFINE('DB_NAME','fyet-db');
+DEFINE('DB_NAME','fyet-db');*/
 
 $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
     or die("Could not connect to MySql Database: " . mysqli_connect_error());
 
-// Test connection is established    
+// Test connection is established
 //echo "Connected to MySql Database";
 
 ?>

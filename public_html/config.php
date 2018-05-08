de1d378519207900ea4ad1debb4e1b8ed17bde33
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-07 18:05:21 -0800

    Fixed accidental changes DB config file.

--- public_html/config.php
+++ public_html/config.php
@@ -1,14 +1,14 @@
 <?php
 
-DEFINE('DB_USER','fyet-db');
-DEFINE('DB_PASSWORD','FZYLHbqfslqyA1WB');
-DEFINE('DB_HOST','oniddb.cws.oregonstate.edu');
-DEFINE('DB_NAME','fyet-db');
+DEFINE('DB_USER','root');
+DEFINE('DB_PASSWORD','camelopardalis');
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_NAME','camelopardalis');
 
 $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
     or die("Could not connect to MySql Database: " . mysqli_connect_error());
 
-// Test connection is established    
+// Test connection is established
 //echo "Connected to MySql Database";
 
 ?>

33f18d5c0531546add74be35c62939882acf92e1
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-13 01:10:49 -0800

    Fixed misplaced parenthesis.

--- public_html/admin-side/scripts/activityTable.js
+++ public_html/admin-side/scripts/activityTable.js
@@ -14,24 +14,25 @@ $(document).ready(function(){
     //Send request to reload table
     loadTable(newStart, newEnd);
   });
+
+
+  function loadTable(startDate, endDate){
+    $.get('components/activityTable.php', {start:startDate, end:endDate}, function(data){
+      $('#activityTable').html(data);
+    });
+  }
+
+  function currentDate(){
+    var d = new Date();
+    //Get current day of the month and pad with 0 as necessary
+    var day = d.getDate().toString();
+    if (day < 10)
+      day = '0' + day.toString();
+    //Get current month and pad with 0 as necessary
+    var month = d.getMonth() + 1;
+    if (month < 10)
+      month = '0' + month.toString();
+    //Return date in format 'yyyy/mm/dd'
+    return d.getFullYear().toString() + '-' + month + '-' + day;
+  }
 });
-
-function loadTable(startDate, endDate){
-  $.get('components/activityTable.php', {start:startDate, end:endDate}, function(data){
-    $('#activityTable').html(data);
-  });
-}
-
-function currentDate(){
-  var d = new Date();
-  //Get current day of the month and pad with 0 as necessary
-  var day = d.getDate().toString();
-  if (day < 10)
-    day = '0' + day.toString();
-  //Get current month and pad with 0 as necessary
-  var month = d.getMonth() + 1;
-  if (month < 10)
-    month = '0' + month.toString();
-  //Return date in format 'yyyy/mm/dd'
-  return d.getFullYear().toString() + '-' + month + '-' + day;
-}

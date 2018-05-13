47ede36a7376ad6205918e3909310f7c5a88ce3a
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-13 01:08:24 -0800

    Added card link to BI page.

--- public_html/admin-side/admin-home.php
+++ public_html/admin-side/admin-home.php
@@ -69,7 +69,7 @@
                   </div>
                 </div>
               </a>
-              <a href="#">
+              <a href="admin-biz.php">
                 <div class="card border-light text-white bg-primary mb-3">
                   <div class="card-wrapper">
                     <div class="card-header">Business Insights</div>
@@ -148,8 +148,6 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <!-- Add custom scripts below-->
-  <script type="text/javascript" src="scripts/activityTable.js">
-
-  </script>
+  <script type="text/javascript" src="scripts/activityTable.js"></script>
 </body>
 </html>

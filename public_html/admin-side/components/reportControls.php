ea23aa883bb49cb28ac8cb6ceb62d0ad3b92dcf1
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-13 01:11:40 -0800

    Created separate file for BI control bar code.

---
+++ public_html/admin-side/components/reportControls.php
@@ -0,0 +1,24 @@
+<nav class="navbar fixed-bottom navbar-light bg-dark" id="reportControls">
+  <button type="button"
+          class="btn btn-danger btn-sm"
+          float="right"
+          data-toggle="modal"
+          data-target="#editFiltersModal">
+    Adjust Report Filters
+  </button>
+  <div class="float-right">
+    <a type="button"
+            class="btn btn-success btn-sm"
+            float="right"
+            id="csvBtn">
+      Download Table as .CSV File
+    </a>
+    <a type="button"
+            class="btn btn-success btn-sm"
+            float="right"
+            target="_blank"
+            id="pngBtn">
+      View Chart as .PNG File
+    </a>
+  </div>
+</nav>

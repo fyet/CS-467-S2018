7107b527b4bb92706aee60b95eec0fc20ecc06be
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-16 18:45:22 -0800

    Added icons to user/admin management table buttons.

--- public_html/admin-side/manage-admin.php
+++ public_html/admin-side/manage-admin.php
@@ -72,6 +72,7 @@
                               data-target='#editAdminModal'
                               data-id={$row['id']}
                               data-email={$row['email']}>
+                              <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"14\" height=\"16\" viewBox=\"0 0 14 16\"><path fill-rule=\"evenodd\" d=\"M0 11.592v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3l-1.3 1.3-3-3 1.3-1.3a.996.996 0 0 1 1.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z\"/></svg>
                               Edit
                         </button>
                       </td>
@@ -83,6 +84,7 @@
                               data-target='#deleteAdminModal'
                               data-id={$row['id']}
                               data-email={$row['email']}>
+                              <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"12\" height=\"16\" viewBox=\"0 0 12 16\"><path fill-rule=\"evenodd\" d=\"M7.71 8.23l3.75 3.75-1.48 1.48-3.75-3.75-3.75 3.75L1 11.98l3.75-3.75L1 4.48 2.48 3l3.75 3.75L9.98 3l1.48 1.48-3.75 3.75z\"/></svg>
                               Delete
                         </button>
                       </td>

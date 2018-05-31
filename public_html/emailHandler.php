e6f8608740b192baabff740929324b553eca5122
Matthew Morse <morsmatt@oregonstate.edu>
2018-05-31 15:11:33 -0800

    Updated email handler function in main directory.

--- public_html/emailHandler.php
+++ public_html/emailHandler.php
@@ -1,44 +1,43 @@
-<?php  
+<?php
+// Import necessary PHPmailer classes
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  // Import necessary PHPmailer classes
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
+//subject, body, and certification path are optional arguments
+function emailHandler($recipientEmail,$subject="",$body="",$certPath=""){
 
-  function emailHandler($path,$email){
+      // The code structure below is from phpmailer's manual: https://github.com/PHPMailer/PHPMailer
+      require 'vendor/autoload.php'; // the autoload file
 
-        // The code structure below is from phpmailer's manual: https://github.com/PHPMailer/PHPMailer   
-        require 'vendor/autoload.php'; // the autoload file
+      $mail = new PHPMailer(true);
+      try {
+          //Server settings
+          $mail->SMTPDebug = 0;   // Set this to '2' for logging/debugging details to be printed to browser
+          $mail->isSMTP();
+          $mail->Host = 'smtp.gmail.com';
+          $mail->SMTPAuth = true;
+          $mail->Username = 'camelopardalis.awardhub@gmail.com';
+          $mail->Password = 'cs467group!@#$';
+          $mail->SMTPSecure = 'tls';
+          $mail->Port = 587;
 
-        $mail = new PHPMailer(true);                             
-        try {
-            //Server settings
-            $mail->SMTPDebug = 0;   // Set this to '2' for logging/debugging details to be printed to browser                           
-            $mail->isSMTP();                                        
-            $mail->Host = 'smtp.gmail.com';                       
-            $mail->SMTPAuth = true;                                 
-            $mail->Username = 'camelopardalis.awardhub@gmail.com';  
-            $mail->Password = 'cs467group!@#$';                     
-            $mail->SMTPSecure = 'tls';                              
-            $mail->Port = 587;                                      
+          //Recipients
+          $mail->setFrom('camelopardalis.awardhub@gmail.com', 'Award Hub');
+          $mail->addAddress($recipientEmail);
 
-            //Recipients
-            $mail->setFrom('camelopardalis.awardhub@gmail.com', 'Award Hub');
-            $mail->addAddress($email); 
+          //Attachments
+          if (strcmp($certPath, "") != 0) {
+            $mail->addAttachment($certPath);
+          }
+          //Content
+          $mail->isHTML(true);
+          $mail->Subject = $subject;
+          $mail->Body    = $body;
+          $mail->AltBody = $body;
 
-            //Attachments
-            $mail->addAttachment($path);   
-
-            //Content
-            $mail->isHTML(true);                                 
-            $mail->Subject = 'Congratulations';
-            $mail->Body    = 'You are being recognized for your outstanding achievement! Please accept the attached award certificate as a token of gratitude.';
-            $mail->AltBody = 'You are being recognized for your outstanding achievement! Please accept the attached award certificate as a token of gratitude.';
-
-            $mail->send();
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-        } 
-  }
-?>
-    
+          $mail->send();
+          echo 'Message has been sent';
+      } catch (Exception $e) {
+          echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+      }
+}
